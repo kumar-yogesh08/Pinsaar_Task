3f69e5c044c045a62306773fdac8b4995700a33f
kumar-yogesh08 <157942195+kumar-yogesh08@users.noreply.github.com>
2025-08-18 23:47:16 +0530

    Done:Schema and basic setup

---
+++ api/src/models/Note_Model.js
@@ -0,0 +1,24 @@
+import {mongoose} from 'mongoose';
+
+const AttemptSchema= new mongoose.Schema({
+    at:{type:Date,required:true},
+    statusCode:{type:Number, required:false},
+    ok:{type:Boolean, required:true},
+    error_value:{type:String,required:false}
+},{_id:false});
+
+const NoteSchema = new mongoose.Schema({
+    title:String,
+    body:String,
+    releaseAt:{type:Date,index:true},
+    webhookurl:String,
+    status:{
+        type:String,
+        enum:["pending","delivered","failed","dead"],
+        index:true,
+        default:"pending"
+    },
+    attempts:{type:[AttemptSchema],default:[]},
+    deliveredAt:{type:Date,default:null}
+})
+export default mongoose.model('Note_Model',NoteSchema)
